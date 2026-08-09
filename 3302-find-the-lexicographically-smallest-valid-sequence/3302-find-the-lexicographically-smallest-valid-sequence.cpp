8184928486428ff9962071e620035d850c7b8404
Rishabh kumar <98063676+rishabhk159@users.noreply.github.com>
2026-08-09 16:36:48 +0530

    Time: 39 ms (71.28%), Space: 109.3 MB (53.19%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -1,55 +1,27 @@
 class Solution {
 public:
     vector<int> validSequence(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-
-        vector<int> ans(m);
-
-        // last[j] = latest index in word1 where word2[j]
-        // can be matched while still matching word2[j...]
+        int n = word1.length(), m = word2.length();
         vector<int> last(m, -1);
-
-        int i = n - 1;
         int j = m - 1;
-
-        // Match word2 from right to left.
-        while (i >= 0 && j >= 0) {
-            if (word1[i] == word2[j]) {
+        for (int i = n - 1; i >= 0; --i) {
+            if (j >= 0 && word1[i] == word2[j]) {
                 last[j] = i;
-                j--;
+                j -= 1;
             }
-            i--;
         }
-
-        // We can use at most one mismatch.
-        bool canSkip = true;
+        vector<int> res;
+        int skip = 0;
         j = 0;
-
-        // Greedily choose the smallest possible indices.
-        for (i = 0; i < n; i++) {
-            if (j == m)
-                break;
-
-            // Case 1: exact match
-            if (word1[i] == word2[j]) {
-                ans[j] = i;
-                j++;
-            }
-
-            // Case 2: use our one allowed mismatch
-            else if (canSkip &&
-                     (j == m - 1 || i < last[j + 1])) {
-
-                ans[j] = i;
-                j++;
-                canSkip = false;
+        for (int i = 0; i < n; ++i) {
+            if (j == m) break;
+            if (word1[i] == word2[j] ||
+                (skip == 0 && (j == m - 1 || i < last[j + 1]))) {
+                skip += (word1[i] != word2[j] ? 1 : 0);
+                res.push_back(i);
+                j += 1;
             }
         }
-
-        if (j == m)
-            return ans;
-
-        return {};
+        return j == m ? res : vector<int>();
     }
 };
