bd6b1bc242156d86ec9fc44473720610477dd290
Rishabh kumar <98063676+rishabhk159@users.noreply.github.com>
2026-08-16 07:51:26 +0530

    Time: 7 ms (39.59%), Space: 131.4 MB (6.09%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        vector<int> count(3, 0);
+        for (int x : stones) {
+            count[x % 3]++;
+        }
+
+        if (count[0] % 2 == 0) {
+            return count[1] >= 1 && count[2] >= 1;
+        } else {
+            return abs(count[1] - count[2]) > 2;
+        }
+    }
+};
