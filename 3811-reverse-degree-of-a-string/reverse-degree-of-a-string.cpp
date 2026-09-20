b95717d93df0c33aff957cade4cd8d814d2c2c05
Rishabh kumar <98063676+rishabhk159@users.noreply.github.com>
2026-09-20 14:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,20 +1,16 @@
+#include <string>
+
 class Solution {
 public:
-    int reverseDegree(string s) {
-        // Fast I/O
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-
-        int total = 0;
-        int n = s.length();
-
-        for (int i = 0; i < n; ++i) {
-            // 'a' -> 26, 'b' -> 25, ..., 'z' -> 1
-            int rev_char_val = 26 - (s[i] - 'a');
-            // 1-indexed position in string is (i + 1)
-            total += rev_char_val * (i + 1);
+    int reverseDegree(std::string s) {
+        int totalDegree = 0;
+        
+        for (int i = 0; i < s.length(); ++i) {
+            int charValue = 26 - (s[i] - 'a');
+            int position = i + 1;
+            totalDegree += charValue * position;
         }
-
-        return total;
+        
+        return totalDegree;
     }
 };
