7add8a9673604cea5ea4831e22e3fda382199afd
Rishabh kumar <98063676+rishabhk159@users.noreply.github.com>
2026-09-24 20:09:15 +0530

    Time: 132 ms (60.89%) | Memory: 111.5 MB (88.13%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+
+        if (n == 1) {
+            return 0;
+        }
+
+        // Store all indices for each value
+        unordered_map<int, vector<int>> mp;
+
+        for (int i = 0; i < n; i++) {
+            mp[arr[i]].push_back(i);
+        }
+
+        queue<int> q;
+        vector<bool> visited(n, false);
+
+        q.push(0);
+        visited[0] = true;
+
+        int steps = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+
+            while (size--) {
+                int i = q.front();
+                q.pop();
+
+                // Reached the last index
+                if (i == n - 1) {
+                    return steps;
+                }
+
+                // Jump to i - 1
+                if (i - 1 >= 0 && !visited[i - 1]) {
+                    visited[i - 1] = true;
+                    q.push(i - 1);
+                }
+
+                // Jump to i + 1
+                if (i + 1 < n && !visited[i + 1]) {
+                    visited[i + 1] = true;
+                    q.push(i + 1);
+                }
+
+                // Jump to all indices having the same value
+                if (mp.count(arr[i])) {
+                    for (int j : mp[arr[i]]) {
+                        if (!visited[j]) {
+                            visited[j] = true;
+                            q.push(j);
+                        }
+                    }
+
+                    // Important: don't process this value again
+                    mp[arr[i]].clear();
+                }
+            }
+
+            steps++;
+        }
+
+        return -1;
+    }
+};
