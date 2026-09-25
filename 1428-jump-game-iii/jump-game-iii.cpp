0e34c99acdd292c9cdd2e6ffe64f06d7ac2fcd56
Rishabh kumar <98063676+rishabhk159@users.noreply.github.com>
2026-09-25 18:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (78.89%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
-
-        queue<int> q;
         vector<bool> visited(n, false);
 
+        queue<int> q;
         q.push(start);
         visited[start] = true;
 
@@ -13,21 +12,16 @@ public:
             int i = q.front();
             q.pop();
 
-            // Reached value 0
-            if (arr[i] == 0) {
+            if (arr[i] == 0)
                 return true;
-            }
 
-            // Jump to the right
             int right = i + arr[i];
+            int left = i - arr[i];
 
             if (right < n && !visited[right]) {
                 visited[right] = true;
                 q.push(right);
             }
-
-            // Jump to the left
-            int left = i - arr[i];
 
             if (left >= 0 && !visited[left]) {
                 visited[left] = true;
